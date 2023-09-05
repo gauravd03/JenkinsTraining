b138680ff60b4466e27dc4069378e56643491aaa
Gaurav Dandekar <gauravdandekar0071@gmail.com>
2023-09-05 17:25:23 +0530

    .

--- JenkinsTrainingSeleProject1/src/main/java/com/selenium/JenkinsTrainingSeleProject1/first.java
+++ JenkinsTrainingSeleProject1/src/main/java/com/selenium/JenkinsTrainingSeleProject1/first.java
@@ -1,5 +1,18 @@
 package com.selenium.JenkinsTrainingSeleProject1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class first {
-
+  @Test
+  public void f() {
+	  WebDriverManager.chromedriver().setup();
+	 WebDriver driver=new ChromeDriver();
+	 
+	 driver.get("https://www.google.com/");
+			 
+			 
+  }
 }
